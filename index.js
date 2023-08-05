06527adec2913bd64569a886bc04c37c222e0f74
gaimard <fokofouading@gmail.com>
2023-08-05 21:11:17 +0200

    Initial set up

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const jsdom = require('jsdom');
+
+const {JSDOM} = jsdom;
+
+const app = express();
+
+// const dom = new JSDOM(`<!DOCTYPE html> <p> This is the first time I use Jsdom... </p> `);
+// console.log(dom.window.document.querySelector('p').innerHTML);
+
+const dom = new JSDOM(`<!DOCTYPE html>Hello`);
+dom.serialize()=== "<!DOCTYPE html> <head></head><body>Hello</body></html>";
+
+console.log(dom.serialize());
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+ '/index.html');
+})
+
+const found=document.getElementsByClassName("start").innerHTML;
+console.log(found);
+
+app.get("/change", function(req,res){
+    // const dom = new JSDOM(`<!DOCTYPE html> <p> This is the first time I use Jsdom... </p> `);
+    // dom.window.document.getElementsByClassName("start").innerHTML=`<p> This is the first time I use Jsdom... </p>`;
+
+    // const dom = new JSDOM(`index.html`);
+    // dom.window.document.getElementsByClassName("start").innerHTML=`<p> This is the first time I use Jsdom... </p>`;
+
+
+
+    res.sendFile(__dirname+ '/index.html');
+})
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log('Server has started on port 3000');
+})
