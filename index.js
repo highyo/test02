9879d802490748dffae3f3bb725e7401c1dfefd0
gaimard <fokofouading@gmail.com>
2023-08-05 22:57:33 +0200

    Third part complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const jsdom = require('jsdom');
+const express = require("express");
+const jsdom = require("jsdom");
 
-const {JSDOM} = jsdom;
+const { JSDOM } = jsdom;
 
 const app = express();
 
@@ -9,35 +9,28 @@ const app = express();
 // console.log(dom.window.document.querySelector('p').innerHTML);
 
 const dom = new JSDOM(`<!DOCTYPE html>Hello`);
-dom.serialize()=== "<!DOCTYPE html> <head></head><body>Hello</body></html>";
+dom.serialize() === "<!DOCTYPE html> <head></head><body>Hello</body></html>";
 
 console.log(dom.serialize());
+let recall = new Date().getDate();
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname+ '/index.html');
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-const found=document.getElementsByClassName("start").innerHTML;
+const found = document.getElementsByClassName("start").innerHTML;
 console.log(found);
 
-app.get("/change", function(req,res){
-    // const dom = new JSDOM(`<!DOCTYPE html> <p> This is the first time I use Jsdom... </p> `);
-    // dom.window.document.getElementsByClassName("start").innerHTML=`<p> This is the first time I use Jsdom... </p>`;
+app.get("/change", function (req, res) {
+  // const dom = new JSDOM(`<!DOCTYPE html> <p> This is the first time I use Jsdom... </p> `);
+  // dom.window.document.getElementsByClassName("start").innerHTML=`<p> This is the first time I use Jsdom... </p>`;
 
-    // const dom = new JSDOM(`index.html`);
-    // dom.window.document.getElementsByClassName("start").innerHTML=`<p> This is the first time I use Jsdom... </p>`;
+  // const dom = new JSDOM(`index.html`);
+  // dom.window.document.getElementsByClassName("start").innerHTML=`<p> This is the first time I use Jsdom... </p>`;
 
+  res.sendFile(__dirname + "/index.html");
+});
 
-
-    res.sendFile(__dirname+ '/index.html');
-})
-
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log('Server has started on port 3000');
-})
+app.listen(3000, () => {
+  console.log("Server has started on port 3000");
+});
